bddeb41655d915936678965eade922d0b453bac7
Ted Zhu <tedz2usa@gmail.com>
2016-10-06 08:55:38 -0400

    Made the first line.

--- main.js
+++ main.js
@@ -13,4 +13,14 @@ function init() {
   ctx = canvas.getContext('2d');
   log(ctx);
 
+  print_line(5, 5, 20, 20);
+
+}
+
+function print_line(x1, y1, x2, y2) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.stroke();
+
 }
