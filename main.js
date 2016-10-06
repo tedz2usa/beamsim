cc65ac7e5f59fda31940a90490b08bcd926519d5
Ted Zhu <tedz2usa@gmail.com>
2016-10-06 09:03:43 -0400

    Transform coords.

--- main.js
+++ main.js
@@ -5,22 +5,40 @@ window.onload = init;
 
 var canvas, ctx;
 
+var canvasWidth, canvasHeight, originXOffset, originYOffset;
 
 function init() {
   log("Loaded!");
 
   canvas = document.getElementById("myCanvas");
+  canvasWidth = canvas.width;
+  canvasHeight = canvas.height;
   ctx = canvas.getContext('2d');
   log(ctx);
 
   print_line(5, 5, 20, 20);
+  print_dot(30, 30);
 
+}
+
+// Transform x coordinate.
+function tx(x) {
+  return x;
+}
+
+// Transform y coordinate.
+function ty(y) {
+  return canvasHeight-y;
+}
+
+function print_dot(x, y) {
+  ctx.fillRect(tx(x), ty(y), 1, 1);
 }
 
 function print_line(x1, y1, x2, y2) {
   ctx.beginPath();
-  ctx.moveTo(x1, y1);
-  ctx.lineTo(x2, y2);
+  ctx.moveTo(tx(x1), ty(y1));
+  ctx.lineTo(tx(x2), ty(y2));
   ctx.stroke();
 
 }
