594aac9784482e7e8838e066b0e89165588a4bfe
Ted Zhu <tedz2usa@gmail.com>
2016-10-06 08:52:47 -0400

    Get 2d context.

--- main.js
+++ main.js
@@ -3,8 +3,14 @@ var log = console.log.bind(console);
 
 window.onload = init;
 
+var canvas, ctx;
 
 
 function init() {
   log("Loaded!");
+
+  canvas = document.getElementById("myCanvas");
+  ctx = canvas.getContext('2d');
+  log(ctx);
+
 }
